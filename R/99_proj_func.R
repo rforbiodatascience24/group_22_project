09903afb71ff565724a8ed7d48a88ea7e34c08b9
AnnaRantzau <s215071@dtu.dk>
2024-11-24 12:34:14 +0100

    01_load: more packages added _raw: raw data added 99_proj_func: More added to unzip the file correctly Anna

--- R/99_proj_func.R
+++ R/99_proj_func.R
@@ -5,20 +5,46 @@ create_dir <- function(path){
   }
 }
 
-# Making a function that downloads data from the NCBI server
+# Making a function that downloads data from the NCBI server - NEW
 download_dataset_ncbi <- function(raw_dir) {
+  create_dir(raw_dir)
 
-  file_ncbi_name <- "raw_data_ncbi.txt.gz"           # naming
+  file_ncbi_name <- "raw_data_ncbi.txt.gz"
+  file_ncbi_path <- str_c(raw_dir, file_ncbi_name)
 
-  if (file.exists(str_c(raw_dir, file_ncbi_name)))   # Checks if the file already exists in raw_dir
+  if (file.exists(file_ncbi_path)) {
+    message("File already exists: ", file_ncbi_path)
     return()
+  }
 
   url_ncbi <- "https://ftp.ncbi.nlm.nih.gov/geo/series/GSE18nnn/GSE18044/matrix/GSE18044_series_matrix.txt.gz"
 
-  download.file(url_ncbi, str_c(raw_dir, file_ncbi_name))       # downloading the file from the URL and saves in raw_dir
-  unzip(str_c(raw_dir, file_ncbi_name), exdir=raw_dir)          # unzipping the file
+  # Download the file
+  download.file(url_ncbi, file_ncbi_path)
+  message("File downloaded: ", file_ncbi_path)
 
-
+  # Decompress the .gz file
+  gunzip(file_ncbi_path, remove = FALSE)
+  message("File decompressed: ", gsub("\\.gz$", "", file_ncbi_path))
 }
+
+
+
+
+# Making a function that downloads data from the NCBI server - OLD
+#download_dataset_ncbi <- function(raw_dir) {
+
+  #file_ncbi_name <- "raw_data_ncbi.txt.gz"           # naming
+
+ # if (file.exists(str_c(raw_dir, file_ncbi_name)))   # Checks if the file already exists in raw_dir
+    #return()
+
+ # url_ncbi <- "https://ftp.ncbi.nlm.nih.gov/geo/series/GSE18nnn/GSE18044/matrix/GSE18044_series_matrix.txt.gz"
+
+ # download.file(url_ncbi, str_c(raw_dir, file_ncbi_name))       # downloading the file from the URL and saves in raw_dir
+ # unzip(str_c(raw_dir, file_ncbi_name), exdir=raw_dir)          # unzipping the file
+
+
+#}
 
 
